1ff7fe217874f229feb1eeb974178948d491ac9b
Chris Kirby <chris@infinitusdev.com>
2017-01-31 08:44:38 -0600

    Groups and Users support. V2 Upgrade (#1)
    * added group name and excluded connection support
    * updated to latest version of serilog and signalr
    * added support for multiple groups and users
    * added support for a proxy based sink
    * new extension method for creating the client sink
    * new sink which accepts the uri, hubname, groups and users args to create a client connection which enables logging from other applications.
    * added usage documentation to the readme

--- README.md
+++ README.md
@@ -2,4 +2,66 @@
 
 [![Build status](https://ci.appveyor.com/api/projects/status/292m45fa26x5iyfs/branch/master?svg=true)](https://ci.appveyor.com/project/serilog/serilog-sinks-signalr/branch/master)
 
-A Serilog sink that writes events to SignalR
+A Serilog sink that writes events to a SignalR Hub
+
+## Configuration from hub application
+
+From within the SignalR server application with a hub named MyHub:
+
+```csharp
+var hubContext = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+
+Log.Logger = new LoggerConfiguration()
+.MinimumLevel.Verbose()
+.WriteTo.SignalR(hubContext,
+  Serilog.Events.LogEventLevel.Information,
+  groupNames: new[] { "CustomGroup"}, // default is null
+  userIds: new[] { "JaneD1234" }, // default is null
+  excludedConnectionIds: new[] { "12345", "678910" }) // default is null
+.CreateLogger();
+```
+
+## Configuration from other clients
+
+From any client application with a hub hosted at `http://localhost:8080` and a hub implemented named `MyHub`:
+
+```csharp
+Log.Logger = new LoggerConfiguration()
+.MinimumLevel.Verbose()
+.WriteTo.SignalRClient("http://localhost:8080",
+  Serilog.Events.LogEventLevel.Information,
+  hub: "MyHub" // default is LogHub
+  groupNames: new[] { "CustomGroup"}, // default is null
+  userIds: new[] { "JaneD1234" }) // default is null
+.CreateLogger();
+```
+
+### SignalR Server
+Create a hub class with any name that ends in `Hub` or use the default name `LogHub`. Then create a method named `receiveLogEvent`, which is capable of accepting all data from the sink.
+```csharp
+public class MyHub : Hub
+{
+  public void receiveLogEvent(string[] groups, string[] userIds, Serilog.Sinks.SignalR.Data.LogEvent logEvent)
+  {
+    // send to all clients
+    Clients.All.sendLogEvent(logEvent);
+    // just the specified groups
+	Clients.Groups(groups).sendLogEvent(logEvent);
+    // just the specified users
+    Clients.Users(users).sendLogEvent(logEvent);
+  }
+}
+```
+
+## Receiving the log event
+Set up a SignalR client and subscribe to the `sendLogEvent` method.
+
+```csharp
+var connection = new HubConnection("http://localhost:8080");
+var hubProxy = connection.CreateHubProxy("MyHub");
+
+hubProxy.On<Serilog.Sinks.SignalR.Data.LogEvent>("sendLogEvent", (logEvent) =>
+{
+  Console.WriteLine(logEvent.RenderedMessage);
+});
+```
--- src/Serilog.Sinks.SignalR/LoggerConfigurationSignalRExtensions.cs
+++ src/Serilog.Sinks.SignalR/LoggerConfigurationSignalRExtensions.cs
@@ -34,6 +34,9 @@ namespace Serilog
         /// <param name="batchPostingLimit">The maximum number of events to post in a single batch.</param>
         /// <param name="period">The time to wait between checking for event batches.</param>
         /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <param name="groupNames">Names of the Signalr groups you are broadcasting the log event to. Default is All Groups.</param>
+        /// <param name="userIds">ID's of the Signalr Users you are broadcasting the log event to. Default is All Users.</param>
+        /// <param name="excludedConnectionIds">Signalr connection ID's to exclude from broadcast.</param>
         /// <returns>Logger configuration, allowing configuration to continue.</returns>
         /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
         public static LoggerConfiguration SignalR(
@@ -42,14 +45,51 @@ namespace Serilog
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
             int batchPostingLimit = SignalRSink.DefaultBatchPostingLimit,
             TimeSpan? period = null,
-            IFormatProvider formatProvider = null)
+            IFormatProvider formatProvider = null,
+            string[] groupNames = null,
+            string[] userIds = null,
+            string[] excludedConnectionIds = null)
         {
-            if (loggerConfiguration == null) throw new ArgumentNullException("loggerConfiguration");
-            if (context == null) throw new ArgumentNullException("context");
+            if (loggerConfiguration == null) throw new ArgumentNullException(nameof(loggerConfiguration));
+            if (context == null) throw new ArgumentNullException(nameof(context));
 
             var defaultedPeriod = period ?? SignalRSink.DefaultPeriod;
             return loggerConfiguration.Sink(
-                new SignalRSink(context, batchPostingLimit, defaultedPeriod, formatProvider),
+                new SignalRSink(context, batchPostingLimit, defaultedPeriod, formatProvider, groupNames, userIds, excludedConnectionIds),
+                restrictedToMinimumLevel);
+        }
+
+        /// <summary>
+        /// Adds a sink that writes log events as documents to a SignalR hub. 
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="url">The url of the hub. http://localhost:8080.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink.</param>
+        /// <param name="batchPostingLimit">The maximum number of events to post in a single batch.</param>
+        /// <param name="period">The time to wait between checking for event batches.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <param name="hub">The name of the Signalr hub class. Default is LogHub</param>
+        /// <param name="groupNames">Names of the Signalr groups you are broadcasting the log event to. Default is All Groups.</param>
+        /// <param name="userIds">ID's of the Signalr Users you are broadcasting the log event to. Default is All Users.</param>
+        /// <returns>Logger configuration, allowing configuration to continue.</returns>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+        public static LoggerConfiguration SignalRClient(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string url,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            int batchPostingLimit = SignalRSink.DefaultBatchPostingLimit,
+            TimeSpan? period = null,
+            IFormatProvider formatProvider = null,
+            string hub = "LogHub",
+            string[] groupNames = null,
+            string[] userIds = null)
+        {
+            if (loggerConfiguration == null) throw new ArgumentNullException(nameof(loggerConfiguration));
+            if (url == null) throw new ArgumentNullException(nameof(url));
+
+            var defaultedPeriod = period ?? SignalRSink.DefaultPeriod;
+            return loggerConfiguration.Sink(
+                new SignalRClientSink(url, batchPostingLimit, defaultedPeriod, formatProvider, hub, groupNames, userIds),
                 restrictedToMinimumLevel);
         }
     }
